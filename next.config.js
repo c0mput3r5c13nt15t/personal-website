8b6d9fb2ea9447c741effd015f4670a7b723b695
Paul Maier <pauljustus279@gmail.com>
2023-02-04 17:24:14 +0100

    Fixed next.config.js and moved back dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-const repo = "personal-website";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
