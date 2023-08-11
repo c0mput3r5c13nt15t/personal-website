8c8699cc8419f02655683c125b40735965cc954b
Paul Maier <pauljustus279@gmail.com>
2023-08-11 15:51:49 +0200

    Fixed placement of contact buttons

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -47,7 +47,7 @@ export default function Contact({
       />
       <div className="z-10">
         <h1 className="text-4xl font-bold mb-4 w-full text-center">{title}</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 flex-wrap w-full justify-center">
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 w-fit mx-auto">
           {mail && (
             <a
               aria-label="Mail"
